fcb31db83f2e5b1ddb3fa63626e699726933fbdd
Felipe Guimaraes <felipeguima4@gmail.com>
2021-08-10 18:16:31 -0300

    [+] Starting the work

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(int argc, char **argv){
+
+    if(argc != 5){"Numero de argumentos incorreto para a executacao do programa.\n");
+        return(1);
+    }
+    return 0;
+}
