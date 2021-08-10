94655c88ed63e1ed4529dd3a19cfb2ed31760e3d
Felipe Guimaraes <felipeguima4@gmail.com>
2021-08-10 18:22:32 -0300

    Fixing main

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 
 int main(int argc, char **argv){
 
-    if(argc != 5){"Numero de argumentos incorreto para a executacao do programa.\n");
+    if(argc != 5){
+        printf("Numero de argumentos incorreto para a executacao do programa.\n");
         return(1);
     }
     return 0;
