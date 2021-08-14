f6bc19702d1e1755e3a8bbb77c44bda1e6b3a7a6
Leandro Souza <LMarqu23@its.jnj.com>
2021-08-14 13:26:25 -0300

    main changes

--- src/main.c
+++ src/main.c
@@ -36,92 +36,16 @@ int main(int argc, char **argv){
 	Report report;
 	initializeStructures(&table, &memory, &report, memSize, pageSize);
 
-	// Criando variáveis e determinando o offset ...
-	char rw; // Caractere que representa a operação - 'R' = read ; 'W' = write.
-	char end[7]; // Endereço de memória em hexadecimal.
-	int offset, pagEncontradaEm, pagLivreEm, idAlg;
-	unsigned numPagVirt;
-	unsigned int endInt;
+	// ...
 
-	offset = determinarOffset(numPag); // Necessário para determinar a página virtual
-	pagEncontradaEm = pagLivreEm = idAlg = 0;
+	clock_t start = clock();
 
-	clock_t inicio = clock();
+	// ...
 
-	FILE *lerArq = fopen(arq, "r");
-	if (lerArq != NULL) {
-		while (fscanf(lerArq, "%s %c", end, &rw) != EOF) {
-			
-			end[7] = '\0';
-			endInt = (int) strtol(end, NULL, 16);
-			numPagVirt = endInt >> offset;
-			pagEncontradaEm = encontrarEndereco(tabela, numPag, end);
-      idAlg++;
-
-			if (rw == 'R') {
-				rel.oppValidas ++;
-				rel.leituras ++;
-			}
-			else if (rw == 'W') {
-				rel.oppValidas ++;
-				rel.escritas ++;
-			}
-			else {
-				rel.oppInvalidas ++;
-			}
-
-			// Se não encontrar um endereço para a iteração atual
-      if(pagEncontradaEm == -1){
-        rel.pageFaults ++;
-        pagLivreEm = encontrarPaginaLivre(tabela, numPag);
-				// Se não houver páginas livres
-        if(pagLivreEm == -1){
-          if(rw == 'W'){
-            rel.pagSujas ++;
-          }
-
-					if(!strcmp(alg,"fifo")){
-						pagLivreEm = fifo(tabela, numPag);
-						escreverNaTabela(tabela, numPag, end, numPagVirt);
-						tabela[pagLivreEm].IdAlgoritmo = idAlg;
-					}
-					else if(!strcmp(alg,"lefe")){
-						fprintf(stderr, "Lefe ainda não foi implementado. \n");
-					}
-					else if(!strcmp(alg, "lru")){
-            pagLivreEm = lru(tabela, numPag);
-            escreverNaTabela(tabela, numPag, end, numPagVirt);
-            tabela[pagLivreEm].IdAlgoritmo = idAlg; 
-          }
-        }
-				else{
-					escreverNaTabela(tabela, numPag, end, numPagVirt);
-					if(!strcmp(alg,"fifo") || !strcmp(alg,"lru")) {
-						tabela[pagLivreEm].IdAlgoritmo = idAlg;
-					}
-					// ...
-				}
-      }
-			// Do contrário, houve hits, ou seja achamos o desejado na
-			// pagina encontrada para a iteração atual.
-			else {
-				rel.hits ++;
-				// ...
-			}
-		}
-	}
-	else {
-		fprintf(stderr, "Não foi possível ler o arquivo de entrada.\n");
-		return 1;
-	}
-
-	fclose(lerArq);
-
-	clock_t fim = clock();
-	executionTime = (double)(inicio - fim) / CLOCKS_PER_SEC;
+	clock_t end = clock();
+	executionTime = (double)(start - end) / CLOCKS_PER_SEC;
 
 	// ... Mensagens de saída no formato desejado ...
-
 	fprintf(stdout, "Tempo de execucao = %lf seg\n", executionTime);
 
 	// Armazenando dados para análise de performance.
