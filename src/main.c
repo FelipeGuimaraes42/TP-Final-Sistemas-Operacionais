a024e1a43c529f5c69a40ff492952c1681996aed
Felipe Guimaraes <felipeguima4@gmail.com>
2021-08-14 17:05:40 -0300

    Adds the output template and some minor fixes

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include "../include/main.h"
 
-// Leandro
-
 int main(int argc, char **argv){
     
 	char *alg = argv[1];
-	char *file = argv[2];
+	FILE *file = fopen(argv[2], "r");
 	int pageSize = atoi(argv[3]);
 	int memSize = atoi(argv[4]);
 	double executionTime = 0.0;
+
+	//Validações de entrada
+	if(file == NULL) {
+		printf("ERRO: Arquivo nao existe %s\n", argv[2]);
+		return 1;
+	}
     
 	if(argc != 5){
 		fprintf(stderr, "ERRO: Numero de argumentos incorreto para a executacao do programa.\n");
@@ -34,21 +38,26 @@ int main(int argc, char **argv){
 	PageTable table;
 	Memory memory;
 	Report report;
+
 	initializeStructures(&table, &memory, &report, memSize, pageSize);
 
 	// Algoritmo principal
 
 	clock_t start = clock();
 
-	virtualMemorySimulator(&table, &memory, fopen(file, "r"), alg);
-
-	// ...
+	virtualMemorySimulator(&table, &memory, file, alg);
 
 	clock_t end = clock();
-	executionTime = (double)(start - end) / CLOCKS_PER_SEC;
+	executionTime = (double)(end - start) / CLOCKS_PER_SEC;
 
 	// ... Mensagens de saída no formato desejado ...
 	fprintf(stdout, "Tempo de execucao = %lf seg\n", executionTime);
+  printf("Arquivo de entrada: %s\n", argv[2]);
+	printf("Tamanho da memoria: %s KB\n", argv[4]);
+	printf("Tamanho das paginas: %s KB\n", argv[3]);
+	printf("Tecnica de reposicao: %s\n", argv[1]);
+	printf("Paginas lidas: %d\n", memory.pagesRead);
+	printf("Paginas escritas: %d\n", memory.pagesToWrite);
 
 	// Armazenando dados para análise de performance.
 	FILE *logTempos;
@@ -57,7 +66,10 @@ int main(int argc, char **argv){
     perror("Não foi possível abrir o arquivo que armazena os tempos.\n");
 	}
 	fprintf(logTempos, "%lf, " ,executionTime);
+
+	//Fecha arquivos
 	fclose(logTempos);
+	fclose(file);
 
 	return 0;
 }
