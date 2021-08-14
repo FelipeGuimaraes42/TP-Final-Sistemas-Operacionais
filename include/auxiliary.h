e9a64c4dbf3c58dabe62a1cee945bbf3d5bf3c1e
Leandro Souza <LMarqu23@its.jnj.com>
2021-08-14 10:58:01 -0300

    initial TrueDev structure

--- include/auxiliary.h
+++ include/auxiliary.h
@@ -7,13 +7,23 @@
  * Defines methods that are auxiliary in nature.
  */
 
+typedef struct {
+  int writes;
+  int reads;
+  int hits;
+  int pageFaults;
+  int dirtyPages;
+} Report;
+
+int determinarOffset(int numPaginas);
+
 unsigned getPage (unsigned addr, int offset);
 
 unsigned returnFreeFrame (Memory *memory);
 
 void returnFlagFullFrame (PageTable *table, Memory *memory, unsigned frame);
 
-void initializeStructures (PageTable *table, Memory *memory, int memSize, int pageSize);
+void initializeStructures (PageTable *table, Memory *memory, Report *report, int memSize, int pageSize);
 
 void loadPage (PageTable *table, Memory *memory, unsigned frame, unsigned page);
 
--- src/auxiliary.c
+++ src/auxiliary.c
@@ -21,3 +21,13 @@ void initializeStructures (PageTable *table, Memory *memory, int memSize, int pa
 void loadPage (PageTable *table, Memory *memory, unsigned frame, unsigned page) {
 
 }
+
+int determinarOffset(int numPaginas) {
+  int offset = 0;
+  int tmp = numPaginas;
+  while (tmp > 1) {
+    tmp = tmp >> 1;
+    offset ++;
+  }
+  return offset;
+}
